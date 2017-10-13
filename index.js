af417298c0c0d3d798835933dca27145a6abac2d
梁恒瑞 <bjlianghengrui@corp.netease.com>
2017-10-13 17:45:02 +0800

    优化interface version判断, 增加service事件注册机制

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
  * Created by panzhichao on 16/8/2.
  */
 'use strict';
-const net       = require('net');
-const url       = require('url');
+const net = require('net');
+const url = require('url');
 const zookeeper = require('node-zookeeper-client');
-const qs        = require('querystring');
-const reg       = require('./libs/register');
-const decode    = require('./libs/decode');
-const Encode    = require('./libs/encode').Encode;
+const qs = require('querystring');
+const reg = require('./libs/register');
+const decode = require('./libs/decode');
+const Encode = require('./libs/encode').Encode;
 let Java = null; //require('js-to-java');
 
 require('./utils');
 
 let SERVICE_LENGTH = 0;
-let COUNT          = 0;
+let COUNT = 0;
 
 /**
  * @param {Object} opt {conn:'zk.dev.pajkdc.com:2181',
@@ -25,20 +25,20 @@ let COUNT          = 0;
  * @constructor
  */
 
-var NZD                 = function (opt) { 
+var NZD = function (opt) {
   Java = opt.java || Java;
-  const self       = this;
-  this.dubboVer    = opt.dubboVer;
+  const self = this;
+  this.dubboVer = opt.dubboVer;
   this.application = opt.application;
-  this.group       = opt.group;
-  this.timeout     = opt.timeout || 6000;
-  this._root        = opt.root || 'dubbo';
+  this.group = opt.group;
+  this.timeout = opt.timeout || 6000;
+  this._root = opt.root || 'dubbo';
   this.dependencies = opt.dependencies || {};
-  SERVICE_LENGTH    = Object.keys(this.dependencies).length;
-  this.client       = zookeeper.createClient(opt.register, {
+  SERVICE_LENGTH = Object.keys(this.dependencies).length;
+  this.client = zookeeper.createClient(opt.register, {
     sessionTimeout: 30000,
-    spinDelay     : 1000,
-    retries       : 5
+    spinDelay: 1000,
+    retries: 5
   });
 
   this.client.connect();
@@ -53,33 +53,53 @@ NZD.prototype._applyServices = function () {
   const refs = this.dependencies;
   const self = this;
 
-  for (let key in refs) { 
+  for (let key in refs) {
     NZD.prototype[key] = new Service(self.client, self.dubboVer, refs[key], self);
   }
 };
 
 var Service = function (zk, dubboVer, depend, opt) {
-  this._zk        = zk;
-  this._hosts     = [];
-  this._version   = depend.version;
-  this._group     = depend.group || opt.group;
+  this._zk = zk;
+  this._hosts = [];
+  this._version = depend.version;
+  this._group = depend.group || opt.group;
   this._interface = depend.interface;
   this._signature = Object.assign({}, depend.methodSignature);
-  this._root      = opt._root;
+  this._root = opt._root;
+  this._events = {
+    'error': [],
+    'success': []
+  }
 
   this._encodeParam = {
-    _dver     : dubboVer || '2.5.3.6',
+    _dver: dubboVer || '2.5.3.6',
     _interface: depend.interface,
-    _version  : depend.version,
-    _group    : depend.group || opt.group,
-    _timeout  : depend.timeout || opt.timeout
+    _version: depend.version,
+    _group: depend.group || opt.group,
+    _timeout: depend.timeout || opt.timeout
   }
 
   this._find(depend.interface);
 };
 
+Service.prototype.on = function (type, cb) {
+  let events = this._events[type]
+  if (!events) { console.log(`no events named ${type}`); return; }
+  if (cb instanceof Function) {
+    events.push(cb)
+  }
+}
+
+Service.prototype._emit = function (type, obj) {
+  let events = this._events[type]
+  if (!events) { console.log(`no events named ${type}`); return; }
+  events.forEach(function (cb) {
+    cb instanceof Function && cb(obj)
+  })
+}
+
 Service.prototype._find = function (path, cb) {
-  const self  = this;
+  const self = this;
   self._hosts = [];
   this._zk.getChildren(`/${this._root}/${path}/providers`, watch, handleResult);
 
@@ -93,29 +113,32 @@ Service.prototype._find = function (path, cb) {
       if (err.code === -4) {
         console.log(err);
       }
+      self._emit('error', err)
       return console.log(err);
     }
     if (children && !children.length) {
-      return console.log(`can\'t find  the zoo: ${path} group: ${self._group},pls check dubbo service!`);
+      let errMsg = `can\'t find  the zoo: ${path} group: ${self._group},pls check dubbo service!`
+      self._emit('error', errMsg)
+      return console.log(errMsg);
     }
 
     for (let i = 0, l = children.length; i < l; i++) {
       zoo = qs.parse(decodeURIComponent(children[i]));
-      if (zoo.version === self._version && zoo.group === self._group) {
+      if ((zoo.version === 'LATEST' || zoo.version === self._version) && zoo.group === self._group) {
         self._hosts.push(url.parse(Object.keys(zoo)[0]).host);
         const methods = zoo.methods.split(',');
         for (let i = 0, l = methods.length; i < l; i++) {
           self[methods[i]] = (function (method) {
             return function () {
               var args = Array.from(arguments);
-              if(args.length && self._signature[method]){
+              if (args.length && self._signature[method]) {
                 args = self._signature[method].apply(self, args);
-                if(typeof args === 'function') args = args(Java);
+                if (typeof args === 'function') args = args(Java);
               }
               return self._execute(method, args);
             };
           })(methods[i]);
-        } 
+        }
       }
     }
     if (!self._hosts.length) {
@@ -125,6 +148,7 @@ Service.prototype._find = function (path, cb) {
       return cb();
     }
     if (++COUNT === SERVICE_LENGTH) {
+      self._emit('success', self)
       console.log('\x1b[32m%s\x1b[0m', 'Dubbo service init done');
     }
   }
@@ -135,17 +159,17 @@ Service.prototype._flush = function (cb) {
 };
 
 Service.prototype._execute = function (method, args) {
-  const self                = this;
+  const self = this;
   this._encodeParam._method = method;
-  this._encodeParam._args   = args;
-  const buffer              = new Encode(this._encodeParam);
+  this._encodeParam._args = args;
+  const buffer = new Encode(this._encodeParam);
 
   return new Promise(function (resolve, reject) {
     const client = new net.Socket();
-    let host     = self._hosts[Math.random() * self._hosts.length | 0].split(':');
+    let host = self._hosts[Math.random() * self._hosts.length | 0].split(':');
     const chunks = [];
     let heap;
-    let bl       = 16;
+    let bl = 16;
     client.connect(host[1], host[0], function () {
       client.write(buffer);
     });
@@ -162,7 +186,7 @@ Service.prototype._execute = function (method, args) {
     client.on('data', function (chunk) {
       if (!chunks.length) {
         var arr = Array.prototype.slice.call(chunk.slice(0, 16));
-        var i   = 0;
+        var i = 0;
         while (i < 3) {
           bl += arr.pop() * Math.pow(256, i++);
         }
